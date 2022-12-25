727131af23d041eb9dcd12ff80627d5d62555fa9
emr__ <emr__@DESKTOP-1PJOTKT>
2022-12-25 14:14:43 +0300

    Proje dosyası ekle.

---
+++ Mark1/Controllers/AraçlarController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mark1.Models.Entity;
+
+
+
+namespace Mark1.Controllers
+{
+
+     
+    public class AraçlarController : Controller
+    {
+        // GET: Araçlar
+        public ActionResult Index()
+        {
+            return View();
+
+            
+        }
+    }
+}
+
